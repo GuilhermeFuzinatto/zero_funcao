fb0ee5581fe0d858611f70e9d7eb17bc2e02b533
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2024-09-12 12:16:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function bask(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    let delta = (b*b)*(-4*a*c);
+    let rdelta = Math.sqrt (delta);
+    let x1 = (-b+rdelta)/(2*a);
+    let x2 = (-b-rdelta)/(2*a);
+
+    if (delta == 0){
+        alert('sem resolução quando delta é igual a 0');
+    }else if (delta < 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('res').innerText = x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('res').innerText = x1 + ',' + x2;
+    }
+}
